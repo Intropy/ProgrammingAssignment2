4b6c0b2e632c06fafe8274cea2bbddbaec83c0bc
Intropy <Intropy@users.noreply.github.com>
2016-02-04 17:14:20 -0800

    make inverse-caching matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions for making a matrix, calculating, caching and retrieving its inverse.
 
-## Write a short comment describing this function
-
+## Make a matrix with specified initial value. Can be re-set with set, fetched with get,
+## and supports caching via setinverse and getinverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## If present fetch cached inverse, otherwise calulate and cache the inverse.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
 }
